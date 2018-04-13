665a29ea1486dbb04d448941014d5eee02e603f2
Oscar Jorge <oscar.jorge.jorge@hotmail.com>
2018-04-13 17:17:53 +0200

    importing form and router module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 
 import { AppComponent } from './app.component';
 
+// -- routes
+const routes: Routes = [
+  { path: '',  component: HomePageComponent},
+  { path: '**', redirectTo: '/' }
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
